83e0f92b887e4272064cdc645d6d6860f713c782
Ilam Ball <informasiilam@gmail.com>
2025-10-30 08:58:25 +0700

    Terapkan padding 50dp di atas dan penataan tengah untuk Column utama

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -1,8 +1,12 @@
 package com.example.inputpengguna
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -17,4 +21,10 @@ fun FormDataDir(modifier: Modifier)
     var jenis by remember { mutableStateOf(value = "") }
 
     val gender: List<String> = listOf("Laki-laki","Perempuan")
+
+    Column (modifier = Modifier.padding(top = 50.dp),
+        verticalArragement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){
+
+    }
 }
