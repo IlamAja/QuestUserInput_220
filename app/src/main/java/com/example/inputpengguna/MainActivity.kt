8e2c65e6403a77d2a4e409b609d9dde13673b522
Ilam Ball <informasiilam@gmail.com>
2025-11-01 13:36:46 +0700

    Siapkan MainActivity dan struktur dasar aplikasi

--- app/src/main/java/com/example/inputpengguna/MainActivity.kt
+++ app/src/main/java/com/example/inputpengguna/MainActivity.kt
@@ -5,35 +5,33 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.inputpengguna.ui.theme.InputPenggunaTheme
+import com.example.inputpengguna.ui.theme.InputPenggunaTheme // GANTI ini jika nama package/theme Anda berbeda!
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
             InputPenggunaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
 
                     FormDataDir(
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .fillMaxSize()
+
+                            .verticalScroll(rememberScrollState())
                     )
                 }
             }
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun FormDataDirPreview() {
-    InputPenggunaTheme {
-        FormDataDir(Modifier)
     }
 }
